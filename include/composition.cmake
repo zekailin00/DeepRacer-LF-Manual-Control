27279ea6daa6b159f2748b1b63ba81eb958cff19
zekailin00 <zekailin00@gmail.com>
2022-06-27 17:17:56 -0700

    initial commit

---
+++ include/composition.cmake
@@ -0,0 +1,7 @@
+find_package(ament_cmake REQUIRED)
+find_package(deepracer_interfaces_pkg REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} deepracer_interfaces_pkg)
+find_package(servo_pkg REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} servo_pkg)
+find_package(deepracer_joy_control REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} deepracer_joy_control)
---
+++ lfc.sh
@@ -0,0 +1,3 @@
+. ~/ros2_foxy/install/setup.bash
+. /home/admin/Desktop/DeepRacer-Manual-Control-Workspace/install/setup.bash
+lfc -c Main.lf
